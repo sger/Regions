ff9c0dd613b4d674514f8d72e8f0ba935dc38268
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2013-09-30 14:48:15 +0300

    Initial Commit

---
+++ Regions/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Regions
+//
+//  Created by Spiros Gerokostas on 9/30/13.
+//  Copyright (c) 2013 Spiros Gerokostas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
